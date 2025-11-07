a18d031cca82d828393ce25f336b36994139f984
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-07 16:09:46 +0530

    operations operators.js

---
+++ operators.js
@@ -0,0 +1,20 @@
+//Operators Operations
+
+let a = 4 ;
+let b = 9;
+
+// Add two numbers
+console.log(a+b);
+
+//Subtract two numbers
+console.log(a-b);
+
+//Multiply two numbers
+console.log(a*b);
+
+//Compare numbers using == and ===
+let c = 3;
+let d = "3";
+
+console.log(c==d);
+console.log(c===d); // check datatype
