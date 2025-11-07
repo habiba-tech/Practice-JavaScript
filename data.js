e6e30b3d605fd883c01ec1d21ad7f689417cda28
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-07 16:00:52 +0530

    DataTypes data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+//Data Types
+
+//String
+let StudentName ="Habiba";
+
+//Number
+let age = "18";
+
+//Boolean 
+let  isonline = true ;
+
+//Array
+let colors = ["red", "blue", "black"];
+
+//Objects 
+let Student = {
+  Name : "Habiba" ,
+  age : 18 ,
+  city : "Mumbai"
+};
+
+console.log(StudentName);
+console.log(age);
+console.log(isonline);
+console.log(colors);
+console.log("Objects",Student);
