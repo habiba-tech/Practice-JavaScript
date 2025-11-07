35d4b4840fc610ff87e69aa60b8feb2286fe370b
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-07 15:45:54 +0530

    Creating variables script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello World") 
+
+//Create a variables
+let name ="Habiba";
+let age ="18";
+let city = "Mumbai"
+
+const pi = 3.14;
+const country = "India";
+
+console.log(name, age, city);
+console.log(pi, country);
