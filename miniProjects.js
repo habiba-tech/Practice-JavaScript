d5274ab4cc92e044a3079d1f3ac3ab0c7192e4e7
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-07 17:11:05 +0530

    Create miniProjects.js

---
+++ miniProjects.js
@@ -0,0 +1,29 @@
+//Biggest of Two Numbers 
+
+let a = 9;
+let b = 4;
+
+if(a>b){
+  console.log(a,"is Greater");
+}else{
+  console.log(b,"is lesser");
+  }
+
+
+// Voting Eligibility 
+
+let Age = 14;
+
+if(Age >= 18){
+  console.log("Eligible to vote");
+}else{
+  console.log("Not Eligible");
+}
+
+//Multiplication of Table
+
+let n = 6;
+
+for(let i=1; i<=10; i++){
+  console.log(n + " x " + i + " = " + (n * 1 ));
+}
