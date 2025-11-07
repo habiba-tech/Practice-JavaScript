3a2e103f419ef0538ef8284a210701525aa4f661
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-07 16:40:45 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,17 @@
+//Loops 
+
+//Print Number 1 to 20 using for loop
+
+console.log("For loop");
+for(let i=1; i<=20; i++){
+  console.log(i)
+};
+
+//Print Number 1 to 20 using While loop
+
+console.log("While loop");
+let i = 1 ;
+while(i<=20){
+  console.log(i);
+  i++;
+}
