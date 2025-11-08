3f0eb32fb5f44ab5e6972b4531d0faecdbac828d
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-08 17:06:27 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,34 @@
+//Array  
+
+let arr = [10,20,30,40];
+
+//Array creation 
+let Fruits = ["Apple","Mango","Banana"];
+
+let Number = [1,2,45,6,89,9,67];
+
+//Array Access
+console.log(Fruits[0]);
+console.log(Fruits[2]);
+console.log(Number[0]);
+
+//Array Update 
+Fruits[1] = "Orange" ;
+
+//Array Length
+console.log(Fruits.length);
+
+//Adding Elements
+//push() - add at end
+
+Fruits.push("Grapes");
+
+//Unshift() - add at start 
+Fruits.unshift("Kiwi");
+
+//Removing Elementd
+//pop() 
+Fruits.pop();
+
+//shift()
+Fruits.shift();
