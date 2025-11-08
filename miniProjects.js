7cb20184c6c776a9d0e80a3add0eaeb63b112b0d
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-08 15:40:48 +0530

    Update miniProjects.js

--- miniProjects.js
+++ miniProjects.js
@@ -27,3 +27,70 @@ let n = 6;
 for(let i=1; i<=10; i++){
   console.log(n + " x " + i + " = " + (n * 1 ));
 }
+
+
+
+//Function Section 
+//crate a function that print "Welcome to javaScript"
+
+function greet(){
+  console.log("Welcome to Javascript");
+}
+
+//Print the Function
+greet();
+
+//create a Fuction that takes two numbers and return theri sum
+
+function sum(a,b){
+  return a+b;
+};
+let result = sum(10,6);
+console.log(result);
+
+//Craete a function that takes a number and returns Square of it.
+
+function Square(n){
+  return n*n;
+};
+let ans = Square(5);
+console.log(ans);
+
+// Create function name largest(x,y) but returns only biggest number
+
+function largest(x,y){
+  if (x>y){
+    console.log(x,"Largest");
+  }else{
+    console.log(y,"Largest");
+  }
+};
+
+largest(2,3);
+
+//Create a Function that take your name and return "Hello,".
+
+function hello(name){
+  console.log("hello , " + name );
+}
+hello("habiba");
+
+//create an arrow function that multiply two number
+
+let multiply = (n,m) => {
+  return m*n;
+};
+console.log(multiply(10,10));
+
+//Craete a function that checks if a number is :
+
+function checkNum(r){
+  if(r > 0){
+    console.log(r,"Positive Number");
+  }else if(r < 0){
+    console.log(r,"Negative Number");
+  }else {
+    console.log(r,"Zero")
+  };
+};
+checkNum(-7);
