7cc603176931096ac59cde4264f22b72cfbb657b
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-16 13:29:14 +0530

    Question solve  js in Dom

--- DOM/practice.js
+++ DOM/practice.js
@@ -1,1 +1,46 @@
+// Q1
+        function changeText() {
+            document.getElementById("title").innerText = "Welcome to DOM";
+        }
+
+        // Q2
+        function changeColor() {
+            document.getElementById("box").style.background = "yellow";
+        }
+
+        // Q3
+        function showText() {
+            let value = document.getElementById("userInput").value;
+            document.getElementById("output").innerText = value;
+        }
+
+        // Q4
+        function addTask() {
+            let li = document.createElement("li");
+            li.innerText = "New Task";
+            document.getElementById("taskList").appendChild(li);
+        }
+
+        // Q5
+        function removePara() {
+            document.getElementById("info").remove();
+        }
+
+        // Q6
+        function toggleDark() {
+            document.body.classList.toggle("dark");
+        }
+
+        // Q7
+        let count = 0;
+
+        function inc() {
+            count++;
+            document.getElementById("num").innerText = count;
+        }
+
+        function dec() {
+            count--;
+            document.getElementById("num").innerText = count;
+        }
 
