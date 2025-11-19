95bf191ea14a1e40880b007c4d5c36c3b33fb763
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-19 09:04:32 +0530

    Two events on same button

---
+++ EventHandling/Events.js
@@ -0,0 +1,9 @@
+let b = document.getElementById("btn2");
+
+b.addEventListener("click", function(){
+    console.log("Clicked");
+});
+
+b.addEventListener("mouseover", function(){
+    console.log("Mouse Over");
+});
