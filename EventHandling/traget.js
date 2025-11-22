0bd10080583b7bfec42f10e0f463664dd6aa8ebc
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-22 16:56:45 +0530

    event.target vs event.currentTarget

--- EventHandling/traget.js
+++ EventHandling/traget.js
@@ -1,8 +1,5 @@
->
-
-<script>
 document.getElementById("box").addEventListener("click", function(event){
     console.log("Target:", event.target.tagName);       // element actually clicked
     console.log("CurrentTarget:", event.currentTarget.id); // element with listener
 });
-</script>
+
