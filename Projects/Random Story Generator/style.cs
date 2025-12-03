8b2c98286dbba4c6d81c4bb859a96bc379647f0f
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-12-03 19:47:00 +0530

    Create style.cs

---
+++ Projects/Random Story Generator/style.cs
@@ -0,0 +1,26 @@
+body{
+    font-family: Arial;
+    text-align: center;
+    padding: 40px;
+    background: #f9f9f9;
+}
+
+#storyBox{
+    width: 80%;
+    margin: auto;
+    margin-top: 30px;
+    padding: 20px;
+    font-size: 20px;
+    background: white;
+    border-radius: 12px;
+    min-height: 120px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.2);
+}
+
+#btn{
+    margin-top: 30px;
+    padding: 12px 25px;
+    font-size: 18px;
+    cursor: pointer;
+    border-radius: 8px;
+}
